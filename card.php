ad4b157bac1d6f3bf8120b1dc319140fe206d843
Emmily Mirelly Menezes Leal <116301152+emmenStar@users.noreply.github.com>
2023-10-31 18:05:49 -0400

    Create card.php

---
+++ card.php
@@ -0,0 +1,39 @@
+* {
+  margin: 0%;
+  padding: 0%;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+
+}
+
+body {
+
+  background-color: rgb(174, 221, 205);
+
+}
+
+.container {
+  width: 100%;
+  padding: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+}
+
+.container .card {
+  display: flex;
+  position: relative;
+  padding: 5px;
+  width: 500px;
+  background-color: rgb(236, 236, 236);
+  margin: 40px;
+  border: 1px solid black;
+  border-radius: 20px;
+  overflow: hidden;
+  transition: 0.5s;
+  text-align: center;
+
+
+}
