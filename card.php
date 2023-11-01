018d8d5e46ba6687fe6aa3f2af37e51be1d1dcc9
Emmily Mirelly Menezes Leal <116301152+emmenStar@users.noreply.github.com>
2023-11-01 18:02:26 -0400

    Update card.php

--- card.php
+++ card.php
@@ -1,39 +1,221 @@
-* {
-  margin: 0%;
-  padding: 0%;
-  box-sizing: border-box;
-  font-family: Arial, Helvetica, sans-serif;
+<!DOCTYPE html>
+<html lang="pt - br">
 
-}
+<head>
+  <meta charset="=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" contaent="IE=edge">
+  <meta name="viewport" content="whidth=device-whidth, initial-scale=1.0">
+  <title>Good.pet</title>
+  <link rel="stylesheet" href="css/Homepage2.css">
+  <link rel="stylesheet" href="card.css">
+  <link rel="stylesheet" href="css/cadastro.html">
+  <script src="../goodpet/src/homepage3.js" defer>
 
-body {
+  </script>
+</head>
 
-  background-color: rgb(174, 221, 205);
+<body>
+  <header>
+    <nav>
+      <a class="logo" href="">
+        <img src="src/logo (1).png">
+      </a>
 
-}
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+      <ul class="nav-list">
+        <li><a href="homePage.html">HOME</a></li>
+        <li><a href="sobre.html">SOBRE</a></li>
+        <li><a href="perdios.html">PERDIDOS</a></li>
+        <li><a href="adoção.html">ADOÇÃO</a></li>
+        <li><a href="contato.html">CONTATO</a></li>
+      </ul>
 
-.container {
-  width: 100%;
-  padding: 25px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-
-}
-
-.container .card {
-  display: flex;
-  position: relative;
-  padding: 5px;
-  width: 500px;
-  background-color: rgb(236, 236, 236);
-  margin: 40px;
-  border: 1px solid black;
-  border-radius: 20px;
-  overflow: hidden;
-  transition: 0.5s;
-  text-align: center;
+      <!--<div class="toggle_btn">
+        <i class="fa-regular f a bars" i>
+      </div>
+      <a class="logo" href="servicos.html">
+        <img src="src/usuario.png" height="90px">
+        </ul>-->
+      <div class="btn">PERFIL</div>
+      <a href="perfil.html"></a>
 
 
-}
+
+    </nav>
+  </header>
+  <div class="slider-container">
+    <div class="slider">
+      <div class="slides">
+        <input type="radio" name="radio-btn" id="radio1">
+        <input type="radio" name="radio-btn" id="radio2">
+        <input type="radio" name="radio-btn" id="radio3">
+
+
+        <div class="slide first">
+          <img src="src/slider1.png " alt="">
+        </div>
+        <div class="slide">
+          <img src="src/slider2 (1).png " alt="">
+        </div>
+        <div class="slide">
+          <img src="src/carrossel3.png" alt="">
+        </div>
+        <div class="navigation-auto">
+          <div class="auto-btn1"></div>
+          <div class="auto-btn2"></div>
+          <div class="auto-btn3"></div>
+        </div>
+      </div>
+      <div class="navigation-bar">
+        <label class="manual-btn" for="radio1"></label>
+        <label class="manual-btn" for="radio2"></label>
+        <label class="manual-btn" for="radio3"></label>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+
+  <div class="a">
+    <figure class="profile-card">
+
+      <div class="profile-header">
+        <img src="" alt="Capa Perfil">
+      </div>
+
+      <div class="profile-body">
+
+        <div class="img-perfil">
+          <img src="src/capa-perfil.png" title="Loop Nerd" alt="perfil">
+        </div>
+
+        <div class="titulo">seu pet é seu melhor amigo</div>
+        <figcaption class="descricao">
+
+          <p>
+
+            <b>a</b><br>
+            “Ninguém como um cachorro sabe apreciar a natureza extraordinária da sua conversa.”<br>
+            Para Agilizar o Seu Tempo.
+
+          </p>
+
+        </figcaption>
+
+        <div class="redes-sociais">
+
+          <ul>
+
+            <li><a href="#" title="Facebook"> <i class="icon icon-facebook"></i> </a></li>
+            <li><a href="#" title="Instagram"> <i class="icon icon-instagram"></i> </a></li>
+            <li><a href="#" title="Twitter"> <i class="icon icon-twitter"></i> </a></li>
+            <li><a href="#" title="Pinterest"> <i class="icon icon-pinterest"></i> </a></li>
+
+          </ul>
+        </div>
+      </div>
+
+
+    </figure><!--Profile Card-->
+
+    <figure class="profile-card">
+
+      <div class="profile-header">
+        <img src="uploads/capa-perfil.png" alt="Capa Perfil">
+      </div>
+
+      <div class="profile-body">
+
+        <div class="img-perfil">
+          <img src="" title="Loop Nerd" alt="perfil">
+        </div>
+
+        <div class="titulo">cuide do seu pet</div>
+        <figcaption class="descricao">
+
+          <p>
+
+            <b>b</b><br>
+            “Ninguém como um cachorro sabe apreciar a natureza extraordinária da sua conversa.”<br>
+            Para Agilizar o Seu Tempo.
+
+          </p>
+
+        </figcaption>
+
+        <div class="redes-sociais">
+
+          <ul>
+
+            <li><a href="#" title="Facebook"> <i class="icon icon-facebook"></i> </a></li>
+            <li><a href="#" title="Instagram"> <i class="icon icon-instagram"></i> </a></li>
+            <li><a href="#" title="Twitter"> <i class="icon icon-twitter"></i> </a></li>
+            <li><a href="#" title="Pinterest"> <i class="icon icon-pinterest"></i> </a></li>
+
+          </ul>
+        </div>
+      </div>
+
+
+    </figure><!--Profile Card-->
+
+    <figure class="profile-card">
+
+      <div class="profile-header">
+        <img src="uploads/capa-perfil.png" alt="Capa Perfil">
+      </div>
+
+      <div class="profile-body">
+
+        <div class="img-perfil">
+          <img src="" title="Loop Nerd" alt="perfil">
+        </div>
+
+        <div class="titulo">ame seu pet</div>
+        <figcaption class="descricao">
+
+          <p>
+
+            <b>c</b><br>
+            “Ninguém como um cachorro sabe apreciar a natureza extraordinária da sua conversa.”<br>
+            Para Agilizar o Seu Tempo.
+
+          </p>
+
+        </figcaption>
+
+        <div class="redes-sociais">
+
+          <ul>
+
+            <li><a href="#" title="Facebook"> <i class="icon icon-facebook"></i> </a></li>
+            <li><a href="#" title="Instagram"> <i class="icon icon-instagram"></i> </a></li>
+            <li><a href="#" title="Twitter"> <i class="icon icon-twitter"></i> </a></li>
+            <li><a href="#" title="Pinterest"> <i class="icon icon-pinterest"></i> </a></li>
+
+          </ul>
+        </div>
+      </div>
+
+
+    </figure><!--Profile Card-->
+
+
+
+  </div>
+
+  </div>
+</body>
+
+</html>
+</div>
+</body>
+
+</html>
